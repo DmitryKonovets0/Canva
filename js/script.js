e70e3698bd44640457397e75ff311d59fbbce6a3
Dmitry Konovets <114726643+DmitryKonovec0@users.noreply.github.com>
2023-02-02 00:28:21 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var canvas = document.getElementById('canvas')
+var ctx = canvas.getContext('2d')
+
+ctx.fillStyle = 'rgb(66,150,168)';
+ctx.fillRect(281,76,496,274);
+
+ctx.fillStyle = 'rgb(125,90,136)';
+ctx.fillRect(777,74,383,277);
+
+ctx.fillStyle = 'rgb(177,156,184)';
+ctx.fillRect(281,352,388,257);
+
+ctx.fillStyle = 'rgb(151,193,150)';
+ctx.fillRect(671, 353,488,256);
+
+ctx.fillStyle = 'rgb(74,138,88)';
+ctx.fillRect(281,614,200,128);
+
+ctx.fillStyle = 'rgb(77,173,173)';
+ctx.fillRect(478,609,203,138);
+
+ctx.fillStyle = 'rgb(217,217,217)';
+ctx.fillRect(685,612,473,136);
